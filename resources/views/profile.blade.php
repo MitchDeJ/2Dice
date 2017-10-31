7c2c8c70f7fa7af6e735ff146e9ae1684e5c9961
Ruben Catshoek <rcatshoek@student.scalda.nl>
2017-10-31 23:14:41 +0100

    Added bootstrap, added app blade, added profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ruben
+ * Date: 31-10-2017
+ * Time: 21:46
+ */
+?>
+    <div class="container-fluid">
+        <div class="card mb-3">
+            <div class="card-header"><i class="fa fa-user"></i> Envy's profile | <a class="text-dark" href="#"> Edit profile</a></div>
+            <div class="card-body">
+                <img src="img/placeholder.png" width="200px" height="200px" class="img-thumbnail" style="display: block; margin: auto; margin-bottom: 1%">
+                <h4 class="text-center"><strong>Billionaire</strong> Envy</h4>
+
+                <div class="form-group">
+                    <label for="about_area"><h5>About me</h5></label>
+                    <textarea class="form-control text-center" id="about_area" rows="3" disabled>Lorem ipsum</textarea>
+                </div>
+
+                <label for="about_area"><h5>Player information</h5></label>
+
+                <div class="table-responsive table-bordered">
+                    <table class="table table-bordered table-bordered table-striped">
+                        <tbody>
+                        <tr>
+                            <td><i class="fa fa-user"></i> <strong>Leaderboard rank:</strong> #1</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-user"></i> <strong>Power:</strong> 1.555.666</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-home"></i> <strong>Company:</strong> Ome Duo</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-usd"></i> <strong>Cash:</strong> $101.556.677</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-bank"></i> <strong>Bank:</strong> $1.464.634.664.666</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <br> <label for="about_area"><h5>Betting information</h5></label>
+
+                <div class="table-responsive table-bordered">
+                    <table class="table table-bordered table-bordered table-striped">
+                        <tbody>
+                        <tr>
+                            <td><i class="fa fa-user"></i> <strong>Bet stat:</strong> #1</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <br> <label for="about_area"><h5>Object information</h5></label>
+
+                <div class="table-responsive table-bordered">
+                    <table class="table table-bordered table-bordered table-striped">
+                        <tbody>
+                        <tr>
+                            <td><i class="fa fa-globe"></i> <strong>Object(s):</strong> Blackjack Holland |  Roulette Belgium</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
