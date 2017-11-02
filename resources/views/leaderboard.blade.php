e3e73e90aeb47b8fb740f7488feb78c0eb8855f8
Ruben Catshoek <rcatshoek@student.scalda.nl>
2017-11-02 20:17:13 +0100

    Small table fix v2

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -12,43 +12,47 @@
     <div class="container-fluid">
         <div class="card mb-3">
             <div class="card-header"><i class="fa fa-tasks i_button_background"></i> Leaderboard</div>
-                <div class="table-responsive">
+            <div class="table-responsive">
+                <div class="card-body">
                     <div class="col-md-2" style="float: right;">
-                    <label>Search:
-                        {!! Form::open(['route' => ['leaderboard.getPlayer'], 'method' => 'post', 'class' => 'form-horizontal']) !!}
-                        {!! Form::text('name', '', ["class" => "form-control form-control-sm"]) !!}
-                        {!! Form::close() !!}
-                    </label>
+                        <label>Search:
+                            {!! Form::open(['route' => ['leaderboard.getPlayer'], 'method' => 'post', 'class' => 'form-horizontal']) !!}
+                            {!! Form::text('name', '', ["class" => "form-control form-control-sm"]) !!}
+                            {!! Form::close() !!}
+                        </label>
 
-                    <label>Page
-                        {!! Form::open(['route' => ['leaderboard.getPage'], 'method' => 'post', 'id' => 'pageselect']) !!}
-                        <select name="pageselected" class="form-control form-control-sm" form="pageselect" onchange="this.form.submit()">
-                            @for($i=1; $i<=$pages; $i++)
-                            <option value="{{$i}}" @if ($i == $num) selected="selected"@endif>{{$i}}</option>
+                        <label>Page
+                            {!! Form::open(['route' => ['leaderboard.getPage'], 'method' => 'post', 'id' => 'pageselect']) !!}
+                            <select name="pageselected" class="form-control form-control-sm" form="pageselect"
+                                    onchange="this.form.submit()">
+                                @for($i=1; $i<=$pages; $i++)
+                                    <option value="{{$i}}" @if ($i == $num) selected="selected"@endif>{{$i}}</option>
                                 @endfor
-                        </select>
-                        {!! Form::close() !!}
-                    </label>
+                            </select>
+                            {!! Form::close() !!}
+                        </label>
                     </div>
                     <table class="table table-striped">
                         <thead>
                         <tr>
                             <td class="table_dark_bg" style="width: 2%;">Rank</td>
-                            <td class="table_dark_bg"style="width: 10%;">Player</td>
+                            <td class="table_dark_bg" style="width: 10%;">Player</td>
                             <td class="table_dark_bg" style="width: 10%;">Power</td>
                         </tr>
                         </thead>
                         <tbody>
                         @foreach ($users as $user)
-                        <tr>
-                            <td>{{(($num-1)*25)+$loop->iteration}}</td>
-                            <td><a href="{{url("/profile/".$user->name)}}" class="text-dark">{{$user->name}}</a></td>
-                            <td>{{number_format($user->power)}}</td>
-                        </tr>
+                            <tr>
+                                <td>{{(($num-1)*25)+$loop->iteration}}</td>
+                                <td><a href="{{url("/profile/".$user->name)}}" class="text-dark">{{$user->name}}</a>
+                                </td>
+                                <td>{{number_format($user->power)}}</td>
+                            </tr>
                         @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
+    </div>
 @endsection
