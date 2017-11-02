472330e8456ce70b9a3f159189cb464029ca089c
Ruben Catshoek <rcatshoek@student.scalda.nl>
2017-11-02 01:18:44 +0100

    Added leaderboard view

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -12,8 +12,39 @@
     <div class="container-fluid">
         <div class="card mb-3">
             <div class="card-header"><i class="fa fa-tasks i_button_background"></i> Leaderboard</div>
-            <div class="card-body">
-            </div>
+                <div class="table-bordered table-responsive">
+                    <div class="col-md-2" style="float: right;">
+                    <label>Search:
+                        <input type="search" class="form-control form-control-sm" placeholder="" aria-controls="dataTable">
+                    </label>
+
+                    <label>Page
+                        <select name="dataTable_length" aria-controls="dataTable" class="form-control form-control-sm">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </label>
+                    </div>
+
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                        <tr>
+                            <td>Rank</td>
+                            <td>Player</td>
+                            <td>Power</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td><a href="#" class="text-dark">Envy</a></td>
+                            <td>{{number_format($user->power)}}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
         </div>
     </div>
+</div>
 @endsection
