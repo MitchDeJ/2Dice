6d5047a040b23dc5e418d9f0ae03d0b5c8e4fd0d
Ruben Catshoek <rcatshoek@student.scalda.nl>
2017-11-11 23:25:32 +0100

    amount of prestige points added to sohp

--- resources/views/prestige.blade.php
+++ resources/views/prestige.blade.php
@@ -13,6 +13,7 @@
         <div class="card mb-3">
             <div class="card-header"><i class="fa fa-shopping-bag i_button_background"></i> Prestige shop</div>
             <div class="card-body">
+                <p>You currently have {{number_format($user->prestigepoints)}} prestige points.</p>
                 <p>The prestige shop accepts prestige points only. Click <a href="{{ url('/general') }}" class="text-dark"><b>here</b></a> to go to the general store.</p>
                 <div class="table-responsive">
                     <table class="table table-striped">
