880e9a21851b8a2b905c87629f498933a27f0ee8
BuildTools <mitchelldejonge1997@gmail.com>
2017-11-16 15:53:37 +0100

    hidden card fix

--- app/Http/Controllers/BlackjackController.php
+++ app/Http/Controllers/BlackjackController.php
@@ -67,8 +67,13 @@ class BlackjackController extends Controller
                 BlackjackController::getCardName($turns[$i]->cpucard) .
                 '.png';
 
-            if ($state == "ONGOING" && $hideCard == -1 && $turns[$i]->cpucard == 0)
-                $hideCard = $i-1;
+            if ($state == "ONGOING" && $hideCard == -1) {
+                if ($turns[$i]->cpucard == 0)
+                $hideCard = $i - 1;
+                else
+                    $hideCard = $turns->count()-1;
+            }
+
 
             if ($turns->count() == 2) {
                 $hideCard = 1;
