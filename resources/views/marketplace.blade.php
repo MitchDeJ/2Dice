640d2fb8a8f673157fa07e355e37c097f31b511a
BuildTools <mitchelldejonge1997@gmail.com>
2017-11-23 22:50:10 +0100

    added if statement to hide table if theres no auctions

--- resources/views/marketplace.blade.php
+++ resources/views/marketplace.blade.php
@@ -15,7 +15,7 @@
         var countDownDate{{$loop->iteration}} = {{$a->end}};
 
         // Update the count down every 1 second
-        var x{{$loop->iteration}} = setInterval(function() {
+        var x{{$loop->iteration}} = setInterval(function () {
 
             // Get todays date and time
             var now = Math.round((new Date()).getTime() / 1000);
@@ -64,12 +64,14 @@
                                     <tbody>
                                     <tr>
                                         <td>
-                                            {{$itemnames->get($offer->item)}} - <b>${{number_format($offer->price)}}</b> each
+                                            {{$itemnames->get($offer->item)}} - <b>${{number_format($offer->price)}}</b>
+                                            each
                                         </td>
                                     </tr>
                                     <tr>
                                         <td>
-                                            {{number_format($offer->completed)}}/{{number_format($offer->amount)}} completed
+                                            {{number_format($offer->completed)}}/{{number_format($offer->amount)}}
+                                            completed
                                         </td>
                                     </tr>
                                     <tr>
@@ -90,15 +92,15 @@
                                     <tr>
                                         <td>
                                             <div class="row" style="margin: auto; justify-content: center">
-                                            {!! Form::open(['route' => ['market.collect'], 'method' => 'post']) !!}
-                                            <button type="submit" class="btn btn-dark form-group">Collect</button>
-                                            {!! Form::hidden("id", $offer->id) !!}
-                                            {!! Form::close() !!}
+                                                {!! Form::open(['route' => ['market.collect'], 'method' => 'post']) !!}
+                                                <button type="submit" class="btn btn-dark form-group">Collect</button>
+                                                {!! Form::hidden("id", $offer->id) !!}
+                                                {!! Form::close() !!}
                                                 &nbsp;
-                                            {!! Form::open(['route' => ['market.cancel'], 'method' => 'post']) !!}
-                                            <button type="submit" class="btn btn-dark form-group">Cancel</button>
-                                            {!! Form::hidden("id", $offer->id) !!}
-                                            {!! Form::close() !!}
+                                                {!! Form::open(['route' => ['market.cancel'], 'method' => 'post']) !!}
+                                                <button type="submit" class="btn btn-dark form-group">Cancel</button>
+                                                {!! Form::hidden("id", $offer->id) !!}
+                                                {!! Form::close() !!}
                                             </div>
                                         </td>
                                     </tr>
@@ -120,40 +122,44 @@
                     <button type="submit" class="btn btn-default">New auction</button>
                 </a>
                 <p><br>If an object is up for auction, it will be shown here.</p>
-                <div class="table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <td class="table_dark_bg" style="width: 10%;">Auction</td>
-                            <td class="table_dark_bg" style="width: 10%;">Auctioneer</td>
-                            <td class="table_dark_bg" style="width: 20%;">Type</td>
-                            <td class="table_dark_bg" style="width: 20%;">Minimum price</td>
-                            <td class="table_dark_bg" style="width: 20%;">Highest bid</td>
-                            <td class="table_dark_bg" style="width: 20%;">Time left</td>
-                            <td class="table_dark_bg" style="width: 20%;">Bid</td>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($auctions as $a)
-                        <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td><a href="{{url("/profile/".$auctioneers[$a->id])}}" class="text-dark">{{$auctioneers[$a->id]}}</a></td>
-                            <td>{{$types[$a->id]}}</td>
-                            <td>${{number_format($a->minprice)}}</td>
-                            <td>${{number_format($a->bid)}}</td>
-                            <td><p id="demo{{$loop->iteration}}"></p></td>
-                            <td>
-                                {!! Form::open(['route' => ['auction.bid'], 'method' => 'post', 'class' => 'form-inline']) !!}
-                                {!! Form::hidden("id", $a->id) !!}
-                                    <input type="number" class="form-control" placeholder="Amount" id="offer" name="amount">
-                                    <button type="submit" class="btn btn-default">Bid</button>
-                                {!! Form::close() !!}
-                            </td>
-                        </tr>
+                @if (count($auctions) > 0)
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <td class="table_dark_bg" style="width: 10%;">Auction</td>
+                                <td class="table_dark_bg" style="width: 10%;">Auctioneer</td>
+                                <td class="table_dark_bg" style="width: 20%;">Type</td>
+                                <td class="table_dark_bg" style="width: 20%;">Minimum price</td>
+                                <td class="table_dark_bg" style="width: 20%;">Highest bid</td>
+                                <td class="table_dark_bg" style="width: 20%;">Time left</td>
+                                <td class="table_dark_bg" style="width: 20%;">Bid</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($auctions as $a)
+                                <tr>
+                                    <td>{{$loop->iteration}}</td>
+                                    <td><a href="{{url("/profile/".$auctioneers[$a->id])}}"
+                                           class="text-dark">{{$auctioneers[$a->id]}}</a></td>
+                                    <td>{{$types[$a->id]}}</td>
+                                    <td>${{number_format($a->minprice)}}</td>
+                                    <td>${{number_format($a->bid)}}</td>
+                                    <td><p id="demo{{$loop->iteration}}"></p></td>
+                                    <td>
+                                        {!! Form::open(['route' => ['auction.bid'], 'method' => 'post', 'class' => 'form-inline']) !!}
+                                        {!! Form::hidden("id", $a->id) !!}
+                                        <input type="number" class="form-control" placeholder="Amount" id="offer"
+                                               name="amount">
+                                        <button type="submit" class="btn btn-default">Bid</button>
+                                        {!! Form::close() !!}
+                                    </td>
+                                </tr>
                             @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                            </tbody>
+                        </table>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
